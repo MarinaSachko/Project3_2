de0af8597644ad5b476bcdd043642d7ee370ec5e
Marina.Sachko <marina.sachko@mail.ru>
2023-04-24 21:22:55 +0300

    initialization_2 commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int account; int Sum_add; int prezent; int quantity_for_prezent, Sum_bonus;
-            account = 1500;                                           //первоначальный счет
+            account = 100;                                           //первоначальный счет
             prezent = 1;                                              //количество бонусных рублей
             quantity_for_prezent = 100;                               //за какое количетво рублей дается prezent бонусов
-            Sum_add = 500;                                            //сумма пополнения
+            Sum_add = 1100;                                            //сумма пополнения
 
             Sum_bonus = (Sum_add>1000) ? Sum_add / 100 * prezent : 0; //сумма бонусных рублей
 
